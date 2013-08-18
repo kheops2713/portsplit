cd7937231c7958bbc2bda4d78a135e53dfa58625
KheOps <kheops@ceops.eu>
2013-08-18 20:54:37 +0000

    Correct introductory comments in the file - it stated a wrong filename from an old version of the code

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 /*
- * ps2.c - this file is part of Portsplit.
+ * main.c - this file is part of Portsplit.
  *
  * Portsplit is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
